533e1802cb539c14d9b56855b86c706cf8e7f687
Fernando Zendron <fearzendron@gmail.com>
2011-04-13 22:34:20 -0300

    Sincronizando

--- wp-content/themes/jzconsultoria/home.php
+++ wp-content/themes/jzconsultoria/home.php
@@ -1,6 +1,160 @@
 <?php get_header('home'); ?>
 	 
-	Home
+<div class="img_people img_people_size">
+	<div class="img_black_box img_black_box_size"></div>
+	<div class="img_black_box_back img_black_box_back_size"></div>
+	<span class="jargao">A melhor consultoria e treinamento em Gestão da Qualidade</span>
+	<span class="jargao_frase">O sistema de Gestão da Qualidade, adequadamente planejado e controlado, é a base para a<br />sustentabilidade de qualquer organização.</span>
+</div>
+<div class="img_linha_base img_linha_base_size">
+
+<div class="jz_descricao">
+	<div class="descricao">
+		<p>Qualquer que seja o tipo de sua organização, uma indústria, uma escola, um comércio de
+	varejo, um consultório médico, etc. a competição está sempre presente. Os clientes estão
+	cada vez mais exigentes e, na maioria das vezes, tem diversas opções de fornecedores para
+	satisfazer a sua necessidade de compra de produtos ou serviços.</p><br />
+
+		<p>A gestão adequada da qualidade de seus processos é a base para o crescimento e
+	perpetuação de sua organização no mercado assim como a gestão adequada dos aspectos
+	ambientais presentes nos seus processos é a garantia da perpetuação do meio ambiente e da
+	sustentabilidade do seu meio de vida para as gerações vindouras.</p><br />
+
+		<p>A <b>J.ZENDRON</b> pode assessorar a sua empresa nessa caminhada.</p>
+		
+	</div>
+</div>
+
+<div class="consultoria">
+	<div class="consul_title">
+		<div class="consul_title_text">Consultoria</div>
+		<div class="line_consul img_line_topics img_line_topics_size"></div>
+	</div>
+	<div class="consul_conteudo">
+		<div class="consul_conteudo_texto">
+			<h3><b>ISO 9001</b></h3>
+			<br />
+			<p>Todos os processos que tem impacto direto na satisfação do cliente devem ser controlados e
+			monitorados de forma adequada e de acordo com os requisitos da norma ISO 9001.</p><br />
+
+			<p>A J.ZENDRON participa de todas as fases do processo, auxiliando na busca de soluções para o
+			controle adequado do processo, elaboração da documentação, treinamento dos funcionários e
+			processo de certificação.</p><br />
+
+			<p>Os serviços de consultoria abrangem as seguintes fases;</p><br />
+
+			<ul>
+				<li>Diagnóstico;</li>
+				<li>Planejamento do projeto de implantação;</li>
+				<li>Formação de grupos de trabalho;</li>
+				<li>Conscientização de todos os funcionários para a qualidade;</li>
+				<li>Elaboração de documentos;</li>
+				<li>Treinamentos relativos a implantação do SGQ;</li>
+				<li>Auditoria interna;</li>
+				<li>Acompanhamento do processo de certificação;</li>
+			</ul><br /><br />
+
+			<h3><b>ISO 14001</b></h3><br />
+
+			<p>Muito tem se falado ultimamente sobre sustentabilidade, preservação do meio ambiente,
+			reciclagem, reaproveitamento e reuso, etc.</p><br />
+
+			<p>A questão sobre o meio ambiente atrai as atenções do mundo inteiro.</p><br />
+
+			<p>Cresce a pressão dos movimentos populares e organismos não governamentais.</p><br />
+
+			<p>Ocorrem grandes acidentes: BHOPAL(Índia), EXXON VALDEZ (Alasca), PETROBRAS (Cubatão/
+			Paraná); BP Plataforma de Petróleo (Golfo do México).</p><br />
+
+			<p>Aumentam as ameaças ambientais (redução da camada de ozônio e Efeito Estufa);</p><br />
+
+			<p>Todas as empresas geram resíduos. Mesmo aquelas que não mantêm processos de produção e
+			que não manuseiam matérias primas.</p><br />
+
+			<p>A J.ZENDRON auxilia a sua organização na identificação dos aspectos ambientais e seus
+			impactos, bem como na busca de soluções para minimizar a geração de resíduos. Também
+			atua no desenvolvimento dos meios adequados e ambientalmente corretos de destinação
+			de resíduos e para assegurar o atendimento aos requisitos legais aplicáveis ao seu negócio,
+			alem de auxiliar na elaboração da documentação necessária, nos treinamentos relativos à
+			implantação e no acompanhamento do processo de certificação.</p><br />
+
+			<p>Os serviços de consultoria abrangem as seguintes fases;</p><br />
+
+			<ul>
+				<li>Diagnóstico;</li>
+				<li>Planejamento do projeto de implantação;</li>
+				<li>Formação de grupos de trabalho;</li>
+				<li>Identificação dos aspectos e dos impactos ambientais;</li>
+				<li>Conscientização de todos os funcionários para a qualidade;</li>
+				<li>Elaboração de documentos;</li>
+				<li>Treinamentos relativos a implantação do SGA;</li>
+				<li>Auditoria interna;</li>
+				<li>Acompanhamento do processo de certificação;</li>
+			</ul><br /><br />
+
+			<a href=""><div class="img_voltar img_voltar_size"></div></a>
+		</div>
+		
+	</div>
+</div>
+
+<div class="treinamento">
+	<div class="trei_title">
+		<div class="trei_title_text">Treinamento</div>
+		<div class="line_trei img_line_topics img_line_topics_size"></div>
+	</div>
+	<div class="trei_conteudo">
+		<div class="trei_conteudo_texto">
+			
+			<p>A J.ZENDRON realiza treinamentos abertos e “in company”, nos temas relativos aos sistemas
+			de gestão da qualidade e ambiental.</p><br />
+
+			<p>Treinamentos:</p><br />
+
+			<ul>
+				<li>INTERPRETAÇÃO E APLICAÇÃO DA NORMA NBR ISO 9001;</li>
+				<li>INTERPRETAÇÃO E APLICAÇÃO DA NORMA NBR ISO 14001;</li>
+				<li>AUDITOR INTERNO;</li>
+				<li>5S;</li>
+				<li>LIDERANÇA;</li>
+				<li>MASP;</li>
+			</ul><br />
+
+			<p>Entre em contato conosco para saber sobre agendamento e detalhes dos treinamentos.</p><br /><br />
+
+			<a href=""><div class="img_voltar img_voltar_size"></div></a>
+
+		</div>
+	</div>
+	
+</div>
+
+<div class="clientes">
+	<div class="cli_title">
+		<div class="cli_title_text">Clientes</div>
+		<div class="line_cli img_line_topics img_line_topics_size"></div>
+	</div>
+	<div class="cli_conteudo">
+		<div class="cli_conteudo_texto">
+
+
+			<a href=""><div class="img_voltar img_voltar_size"></div></a>
+		</div>
+	</div>
+</div>
+
+<div class="contato">
+	<div class="con_title">
+		<div class="con_title_text">Contato</div>
+		<div class="line_con img_line_topics img_line_topics_size"></div>
+	</div>
+	<div class="con_conteudo">
+		<div class="con_conteudo_texto">
+
+			<a href=""><div class="img_voltar img_voltar_size"></div></a>
+		</div>
+	</div>
+</div>
 
 <?php get_footer(); ?>
 
